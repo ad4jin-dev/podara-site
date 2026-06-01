80b9bcec3f51ddcbd74c0f1616f242a80322ba13
ad4jin-dev <ad4jin@gmail.com>
2026-06-01 14:04:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("PODARA final version loaded");
+let lastScroll = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll && currentScroll > 100) {
+    // 向下滚 → 隐藏
+    navbar.classList.add("hide");
+  } else {
+    // 向上滚 → 显示
+    navbar.classList.remove("hide");
+  }
+
+  lastScroll = currentScroll;
+});
